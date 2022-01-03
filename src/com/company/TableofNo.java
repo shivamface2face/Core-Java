6496c8740860528789869e5325c13fe441065c13
Shivam Singh <shivamfaceuit@gmail.com>
2022-01-03 11:09:41 +0530

    table program

--- src/com/company/TableofNo.java
+++ src/com/company/TableofNo.java
@@ -1,7 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class TableofNo {
     public static void main(String[] args) {
-        
+        Scanner in=new Scanner(System.in);
+        int n= in.nextInt();
+        int i=1;
+        while (i<=10){
+            System.out.println(i*n);
+            i++;
+        }
     }
 }
